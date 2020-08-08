faac5c3f68dc1cbe81f91180a677f490b4118b8e
Vlad Grigorescu <grigorescu@gmail.com>
2020-08-08 13:36:01 -0400

    Use --github-token option instead

--- .ci_scripts/test.sh
+++ .ci_scripts/test.sh
@@ -6,10 +6,8 @@ export PATH=/opt/zeek/bin:$PATH
 
 cd /tmp/evil_bit_checker
 
-echo "$TRAVIS_CREDS" > ~/.netrc
-
 echo "Running travis lint..."
-/usr/local/bin/travis login --auto --com
+/usr/local/bin/travis login --github-token $GITHUB_TOKEN --com
 /usr/local/bin/travis lint --com --skip-completion-check --exit-code .travis.yml
 echo "Running zkg test script"
 bash .ci_scripts/test.sh
