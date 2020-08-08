301a0d76f9ab710f372a9ecc44c8be00a1f422ea
Vlad Grigorescu <grigorescu@gmail.com>
2020-08-08 13:31:24 -0400

    Specify travis.com

--- .ci_scripts/test.sh
+++ .ci_scripts/test.sh
@@ -9,8 +9,8 @@ cd /tmp/evil_bit_checker
 echo "$TRAVIS_CREDS" > ~/.netrc
 
 echo "Running travis lint..."
-/usr/local/bin/travis login --auto
-/usr/local/bin/travis lint --skip-completion-check --exit-code .travis.yml
+/usr/local/bin/travis login --auto --com
+/usr/local/bin/travis lint --com --skip-completion-check --exit-code .travis.yml
 echo "Running zkg test script"
 bash .ci_scripts/test.sh
 echo "Tests succeeded."
