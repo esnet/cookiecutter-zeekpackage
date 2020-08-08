edccd3f39f453195ce43b85d2e45df922387b1cd
Vlad Grigorescu <grigorescu@gmail.com>
2020-08-08 12:25:23 -0400

    CI Test: In order to run travis lint non-interactively, we need to use the undocumented --skip-completion-check option

--- .ci_scripts/test.sh
+++ .ci_scripts/test.sh
@@ -7,7 +7,7 @@ export PATH=/opt/zeek/bin:$PATH
 cd /tmp/evil_bit_checker
 
 echo "Running travis lint..."
-/usr/local/bin/travis lint .travis.yml
+/usr/local/bin/travis lint --skip-completion-check --exit-code .travis.yml
 echo "Running zkg test script"
 bash .ci_scripts/test.sh
 echo "Tests succeeded."
