44406e45881cfb734b647d95681c1669d339b2b9
Vlad Grigorescu <grigorescu@gmail.com>
2020-08-11 22:28:09 -0400

    First stab at running resulting workflow

--- .ci_scripts/lint_workflows.sh
+++ .ci_scripts/lint_workflows.sh
@@ -6,7 +6,7 @@ curl -o github-workflow.json https://raw.githubusercontent.com/Logerfo/schemasto
 
 for i in evil_bit_checker/.github/workflows/*.yml
 do
-    cat $i | python3 -c 'import json, sys; from ruamel import yaml ; y=yaml.safe_load(sys.stdin.read()) ; json.dump(y, sys.stdout)' > $i.json
+    cat $i | python3 -c 'import json, sys; from ruamel import yaml ; y=yaml.safe_load(sys.stdin.read()) ; json.dump(y, sys.stdout)' > workflow-lint-$i.json
     echo "Validating GitHub workflow $i"
-    jsonschema -i $i.json github-workflow.json
+    jsonschema -i workflow-lint-$i.json github-workflow.json
 done
--- .ci_scripts/test_workflows.sh
+++ .ci_scripts/test_workflows.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+set -e
+
 ZEEK_VER=$1
 
-grep 'run:' evil_bit_setter/.github/workflows/*
+for i in workflow-lint-*.json
+do
+    for cmd in $(cat "$i")
+    do
+        cd evil_bit_checker
+        cmd=$(echo "$cmd" | sed -e 's/$GITHUB_WORKSPACE/$GITHUB_WORKSPACE\/evil_bit_checker/g')
+        cmd=$(echo "$cmd" | sed -e "s/\${{ matrix.zeek }}/$ZEEK_VER/g")
+        echo "Running command: $cmd"
+        eval "$(printf $cmd)"
+        cd -
+    done
+done
