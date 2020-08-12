f14eb9c90e2b58054767897b9252bdda2c5fd8fa
Vlad Grigorescu <grigorescu@gmail.com>
2020-08-11 21:48:43 -0400

    Add CI scripts

---
+++ .ci_scripts/lint_workflows.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+curl -O github-workflow.json https://raw.githubusercontent.com/Logerfo/schemastore/master/src/schemas/json/github-workflow.json
+
+for i in evil_bit_checker/.github/workflows/*.yml
+do
+    cat $i | python3 -c 'import json, sys; from ruamel import yaml ; y=yaml.safe_load(sys.stdin.read()) ; json.dump(y, sys.stdout)' > $i.json
+    echo "Validating GitHub workflow $i"
+    jsonschema -i $i.json github-workflow.json
+done
---
+++ .ci_scripts/test_workflows.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ZEEK_VER=$1
+
+grep 'run:' evil_bit_setter/.github/workflows/*
