0b1291311d41804ef0824f49f0ff5479f8cc71ec
Vlad Grigorescu <grigorescu@gmail.com>
2020-08-11 22:31:30 -0400

    Fix filename

--- .ci_scripts/lint_workflows.sh
+++ .ci_scripts/lint_workflows.sh
@@ -6,7 +6,8 @@ curl -o github-workflow.json https://raw.githubusercontent.com/Logerfo/schemasto
 
 for i in evil_bit_checker/.github/workflows/*.yml
 do
-    cat $i | python3 -c 'import json, sys; from ruamel import yaml ; y=yaml.safe_load(sys.stdin.read()) ; json.dump(y, sys.stdout)' > workflow-lint-$i.json
+    name=$(basename "$i")
+    cat $i | python3 -c 'import json, sys; from ruamel import yaml ; y=yaml.safe_load(sys.stdin.read()) ; json.dump(y, sys.stdout)' > workflow-lint-$name.json
     echo "Validating GitHub workflow $i"
-    jsonschema -i workflow-lint-$i.json github-workflow.json
+    jsonschema -i workflow-lint-$name.json github-workflow.json
 done
