2c2e6d9264292263f9baa49aac9d284c91ac9dc8
Vlad Grigorescu <grigorescu@gmail.com>
2020-08-11 23:13:07 -0400

    Resulting directory is relative to cwd

--- .ci_scripts/test_workflows.sh
+++ .ci_scripts/test_workflows.sh
@@ -9,7 +9,7 @@ do
     jq -c '.jobs.test.steps[].run' "$i" | egrep . | egrep -v "^null$" | while read -r cmd
     do
         cd evil_bit_checker
-        cmd=$(echo "$cmd" | sed -e 's/$GITHUB_WORKSPACE/$GITHUB_WORKSPACE\/evil_bit_checker/g')
+        cmd=$(echo "$cmd" | sed -e 's/$GITHUB_WORKSPACE/$PWD\/evil_bit_checker/g')
         cmd=$(echo "$cmd" | sed -e 's/${{ matrix.zeek }}/'$ZEEK_VER'/g')
         echo "Running command: $cmd"
         eval "$(printf "$cmd")"
