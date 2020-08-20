36464a2f5ce540b144377e76a16d8ccf31f34c19
Vlad Grigorescu <vlad@es.net>
2020-08-20 11:53:40 -0500

    A few more fixes

--- {{ cookiecutter.project_slug }}/.ci_scripts/test.sh
+++ {{ cookiecutter.project_slug }}/.ci_scripts/test.sh
@@ -3,19 +3,19 @@
 set -e
 
 function debug_and_die {
-    OUTPUT_PATH=$HOME/.zkg/testing/external_dns/clones/external_dns
+    OUTPUT_PATH=$HOME/.zkg/testing/{{ cookiecutter.project_slug }}/clones/{{ cookiecutter.project_slug }}
     if [ -s $OUTPUT_PATH/zkg.test_command.stdout ]; then
 	echo "zkg test command stdout"
 	echo "-----------------------"
 	cat $OUTPUT_PATH/zkg.test_command.stdout
     fi
-    
+
     if [ -s $OUTPUT_PATH/zkg.test_command.stderr ]; then
 	echo "zkg test command stderr"
 	echo "-----------------------"
 	cat $OUTPUT_PATH/zkg.test_command.stderr
     fi
-    
+
     exit 1
 }
 
@@ -26,3 +26,4 @@ zkg test "$PWD" || debug_and_die
 echo "Tests succeeded. Running zkg install..."
 zkg install --force --skiptests "$PWD" || debug_and_die
 echo "Install succeeded."
+`
