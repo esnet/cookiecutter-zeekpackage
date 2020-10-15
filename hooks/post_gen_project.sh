0ab3fc02b4c91d253a785275a89aaa81c2994222
Vlad Grigorescu <vlad@es.net>
2020-10-14 20:07:38 -0500

    Add README

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,3 +4,7 @@ git init
 git add .
 git commit -m "Initial commit [cookiecutter-zeekpackage]"
 
+git checkout --orphan gh-pages
+git reset --hard
+git commit --allow-empty -m "Initializing gh-pages branch"
+git checkout -
