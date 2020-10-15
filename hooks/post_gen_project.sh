da005be013e543e05a0762dd86503b3f8ee99553
Vlad Grigorescu <vlad@es.net>
2020-10-14 20:09:58 -0500

    Fix gh-page create

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,8 +5,6 @@ git add .
 git commit -m "Initial commit [cookiecutter-zeekpackage]"
 
 git checkout --orphan gh-pages
-echo "Fail 1"
 git reset --hard
 git commit --allow-empty -m "Initializing gh-pages branch"
-echo "Fail?"
-git checkout -
+git checkout master
