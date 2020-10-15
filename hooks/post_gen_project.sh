51da1d941ce14fe6fc2a9b61fd6545276338b3c6
Vlad Grigorescu <vlad@es.net>
2020-10-14 20:09:27 -0500

    Debug issue

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,6 +5,8 @@ git add .
 git commit -m "Initial commit [cookiecutter-zeekpackage]"
 
 git checkout --orphan gh-pages
+echo "Fail 1"
 git reset --hard
 git commit --allow-empty -m "Initializing gh-pages branch"
+echo "Fail?"
 git checkout -
